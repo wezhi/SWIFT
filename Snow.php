6b9ffba103942ba416b60d266ac2c0fa63ad3877
wezhi <admin@wezhi12.com>
2015-10-28 17:37:36 +0800

    SWIFTv0
    v0

---
+++ Snow.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: FROST
+ * Date: 2015/10/28
+ * Time: 16:42
+ */
+class Snow
+{
+    /**
+     * 获取IP地址
+     * @return string
+     */
+    function GetIP(){
+        $ip='';
+        IF(Getenv('HTTP_CLIENT_IP') And StrCaseCmp(Getenv('HTTP_CLIENT_IP'),'unknown')){
+            $ip=Getenv('HTTP_CLIENT_IP');
+        }ElseIF(Getenv('HTTP_X_FORWARDED_FOR') And StrCaseCmp(Getenv('HTTP_X_FORWARDED_FOR'),'unknown')){
+            $ip=Getenv('HTTP_X_FORWARDED_FOR');
+        }ElseIF(Getenv('REMOTE_ADDR')And StrCaseCmp(Getenv('REMOTE_ADDR'),'unknown')){
+            $ip=Getenv('REMOTE_ADDR');
+        }ElseIF(isset($_SERVER['REMOTE_ADDR']) And $_SERVER['REMOTE_ADDR'] And StrCaseCmp($_SERVER['REMOTE_ADDR'],'unknown')){
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }Else{
+            $ip='127.0.0.1';
+        }
+        return $ip;
+    }
+
+    /**
+     * 获取IP地址对应的城市信息
+     * @param null $ip
+     * @return mix|string
+     */
+    function GetAddress($ip = null){
+        if ($ip == null){
+            $ip = $this->GetIP();
+        }
+        $address = file_get_contents("http://int.dpool.sina.com.cn/iplookup/iplookup.php?ip=" . $ip);
+//        return $address;
+        $tmp = explode(' ',$address);
+        return count($tmp);
+        foreach ($tmp as $val){
+            $tmp .= $val;
+        }
+        return $tmp;
+    }
+
+    /**
+     * 获取操作系统
+     * @return string
+     */
+    function GetOS(){
+        $OS = "";
+        if(!empty($_SERVER['HTTP_USER_AGENT'])){
+            $OS = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/win/i',$OS)) {
+                $OS = 'Windows';
+            }elseif (preg_match('/mac/i',$OS)) {
+                $OS = 'MAC';
+            }elseif (preg_match('/linux/i',$OS)) {
+                $OS = 'Linux';
+            }elseif (preg_match('/unix/i',$OS)) {
+                $OS = 'Unix';
+            }elseif (preg_match('/bsd/i',$OS)) {
+                $OS = 'BSD';
+            }else {
+                $OS = 'Other';
+            }
+            return $OS;
+        }else{
+            return "获取访客操作系统信息失败！";
+        }
+    }
+
+    /**
+     * 获取浏览器
+     * @return string
+     */
+    function GetBrowser(){
+        if(!empty($_SERVER['HTTP_USER_AGENT'])){
+            $br = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/MSIE/i',$br)) {
+                $br = 'MSIE';
+            }elseif (preg_match('/Firefox/i',$br)) {
+                $br = 'Firefox';
+            }elseif (preg_match('/Chrome/i',$br)) {
+                $br = 'Chrome';
+            }elseif (preg_match('/Safari/i',$br)) {
+                $br = 'Safari';
+            }elseif (preg_match('/Opera/i',$br)) {
+                $br = 'Opera';
+            }else {
+                $br = 'Other';
+            }
+            return $br;
+        }else{return "获取浏览器信息失败！";}
+    }
+
+    /**
+     * 获取浏览器语言
+     * @return string
+     */
+    function GetBrowserLang(){
+        if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
+            $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+            $lang = substr($lang,0,5);
+            if(preg_match("/zh-cn/i",$lang)){
+                $lang = "简体中文";
+            }elseif(preg_match("/zh/i",$lang)){
+                $lang = "繁体中文";
+            }else{
+                $lang = "English";
+            }
+            return $lang;
+
+        }else{
+            return "获取浏览器语言失败！";
+        }
+    }
+}
--- hello.php
+++ hello.php
@@ -1,3 +1,17 @@
 <?php
-echo 'hello world';
-echo phpinfo();
+require_once("Snow.php");
+
+$snow = new Snow();
+echo $snow->GetOS();
+echo "<br />";
+echo $snow->GetBrowser();
+echo "<br />";
+echo $snow->GetBrowserLang();
+echo "<br />";
+echo $snow->GetIP();
+echo "<br />";
+echo $snow->GetAddress();
+//echo str_replace('	','',$snow->GetAddress("120.25.69.157"));
+
+echo "<hr />";
+echo date('Y-m-d H:i:s',time());
