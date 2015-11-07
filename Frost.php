57bab1a70768b158a7384e2149156689f25435ce
wezhi <admin@wezhi12.com>
2015-11-07 22:58:31 +0800

    常规更新

--- Frost.php
+++ Frost.php
@@ -5,19 +5,13 @@
  * Date: 15/11/5
  * Time: 上午12:16
  */
-class dbPdo{
-	private $pdo;
-	function __construct($dsn,$u,$p) {
-		try{
-			$this->pdo = new PDO($dsn,$u,$p);
-		}catch (PDOException $e){
-			die("Can not link database server" . $e->getMessage());
-		}
-	}
-	function hello(){
-		var_dump($this->pdo);
-	}
-	function getAll($sql){
-		return $this->pdo->query($sql);
-	}
+$dsn = 'mysql:host=120.25.69.157;dbname=cailanzi';
+$ur = 'root';
+$pw = '6RMPKLamQ7rajAZb';
+try{
+	$sfdb = new PDO($dsn,$ur,$pw);
+	//不知道为什么，无效
+//	$sfdb->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+}catch (PDOException $e){
+	die('Connection failed: '.$e->getMessage());
 }
--- hello.php
+++ hello.php
@@ -1,13 +1,65 @@
 <?php
 //require_once("Snow.php");
-require_once("Frost.php");
-
-$db = new dbPdo('mysql:host=120.25.69.157;dbname=cailanzi','root','6RMPKLamQ7rajAZb');
-$db->hello();
-//$db = new PDO('mysql:host=120.25.69.157;dbname=cailanzi','root','6RMPKLamQ7rajAZb');
-//var_dump($db);
-$sql = "select fareaid,fareaname,fareaorder from ecs_sf_area";
-$rows = $db->getAll($sql);
-foreach ($rows as $rw){
-	echo $rw['fareaid'].':'.$rw['fareaname'].':'.$rw['fareaorder']."\n";
+//require_once("Frost.php");
+$dsn = 'mysql:host=localhost;dbname=cdcol';
+$ur = 'root';
+$pw = '97';
+try{
+	$db = new PDO($dsn,$ur,$pw);
+	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+//	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e){
+	die($e->getMessage());
 }
+var_dump($db);
+
+//有try...catch时的事务处理
+try{
+	$db->beginTransaction();
+	$sql = "insert into pdo VALUES (?,?,?)";
+	$stmt = $db->prepare($sql);
+	$stmt->execute(array(null,'a84',3));
+	$stmt->execute(array(null,'a85',4));
+	$stmt->execute(array(null,'a86',5));
+	$db->commit();
+}catch (PDOException $e){
+	$db->rollBack();
+	die($e->getMessage());
+}
+
+
+/** 预处理，直接绑定数据，执行 */
+//$sql = "insert into pdo VALUES (?,?,?)";
+//$stmt = $sfdb->prepare($sql);
+////以数组的方式，比较实用些
+//$res = $stmt->execute(array(null,'abc',33));
+//if ($res){
+//	echo $stmt->rowCount();
+//}else{
+//	echo $stmt->errorInfo();
+//}
+
+
+
+
+//
+//$dsn = 'mysql:host=120.25.69.157;dbname=cailanzi';
+//$ur = 'root';
+//$pw = '6RMPKLamQ7rajAZb';
+//try{
+//	$pdb = new PDO($dsn,$ur,$pw);
+//}catch (PDOException $e){
+//	die('Connection failed: '.$e->getMessage());
+//}
+//
+//
+////$db = new PDO('mysql:host=120.25.69.157;dbname=cailanzi','root','6RMPKLamQ7rajAZb');
+////$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+//var_dump($pdb);
+//$id = 41;
+//$sql = "select * from pdo where id = '$id'";
+///*查询用query*/
+//$rows = $pdb->query($sql);
+//$record = $rows->fetch(PDO::FETCH_ASSOC);
+//echo $record['name'];
+
